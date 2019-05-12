76f1e76cb752f2fbcfa6150ee8a2c9e5655e817a
artem.ovcharenko <artem.ovcharenko@codilime.com>
2019-05-12 17:51:13 +0200

    03-solution

--- exercises/03-decorated.js
+++ exercises/03-decorated.js
@@ -11,5 +11,11 @@ function greeter(name){
   return `hello there, ${name}`;
 }
 
+const old = greeter;
+greeter = function (name) {
+  if(name === 'Artem Ovcharenko') return 'gtfo';
+  return old.apply(this, arguments);
+}
+
 module.exports = greeter;
 
